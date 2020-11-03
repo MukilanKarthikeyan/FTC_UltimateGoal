63f67d71772b3f9b3c2b443918102d8d87015297
ipanchamia22 <46631742+ipanchamia22@users.noreply.github.com>
2020-11-03 18:49:01 -0500

    2020.11.03 started programming testing for the clamp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PrototypingClamp.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class PrototypingClamp extends LinearOpMode {
+
+    Servo ClampRotate = null;
+    Servo Clamp = null;
+    DcMotor ClampArm = null;
+
+    public void runOpMode {
+
+        WaitForStart();
+
+        
+    }
+}
+
