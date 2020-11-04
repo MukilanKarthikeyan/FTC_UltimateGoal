5d67e4cc94b046d04421c10949be2ba1eec8e25b
ipanchamia22 <46631742+ipanchamia22@users.noreply.github.com>
2020-11-04 13:55:43 -0500

    2020.11.04 finished programming the clamp for testing and assigning each of the moving components to buttons on the controller.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PrototypingClamp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PrototypingClamp.java
@@ -6,15 +6,64 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class PrototypingClamp extends LinearOpMode {
 
+    double up;
+    double down;
+
     Servo ClampRotate = null;
     Servo Clamp = null;
     DcMotor ClampArm = null;
 
-    public void runOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException{
 
-        WaitForStart();
+        ClampRotate = hardwareMap.servo.get("ClampRotate");
+        Clamp = hardwareMap.servo.get("Clamp");
+        ClampArm = hardwareMap.dcMotor.get("ClampArm");
 
-        
+        //boolean rotate = false;
+        //double motor = 0.0;
+        //double clamping = 0.0;
+
+        waitForStart();
+
+        while (!isStopRequested()) {
+
+            up = gamepad1.right_trigger + gamepad2.right_trigger;
+            down = gamepad1.left_trigger + gamepad2.left_trigger;
+
+            if(gamepad1.right_trigger > 0.1 || gamepad2.right_trigger > 0.1) {
+                ClampArm.setPower(up);
+            }
+            else if(gamepad1.left_trigger > 0.1 || gamepad2.left_trigger > 0.1) {
+                ClampArm.setPower(-down);
+            }
+            else {
+                ClampArm.setPower(0);
+            }
+
+            if(gamepad1.a || gamepad2.a) {
+                //rotate = true;
+                ClampRotate.setPosition(0.25);
+            }
+
+            if(gamepad1.x || gamepad2.x) {
+                //rotate = false;
+                ClampRotate.setPosition(0.0);
+            }
+
+            if(gamepad1.dpad_up || gamepad2.dpad_up) {
+                Clamp.setPosition(0.4);
+
+            }
+            else if(gamepad1.dpad_down || gamepad2.dpad_down) {
+                Clamp.setPosition(0.0);
+            }
+
+            telemetry.addData("Arm encoder counts:", ClampArm.getCurrentPosition());
+            telemetry.update();
+
+        }
+
     }
 }
 
